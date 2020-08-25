d7240e60902a604cc232631068d86fee86b7597d
Øyvind Rørstad <oyvind.rorstad@skatteetaten.no>
2020-08-25 09:13:30 +0200

    Init commit

---
+++ src/main/resources/run-kafka.sh
@@ -0,0 +1,6 @@
+
+#!/usr/bin/env bash
+cd ~/kafka-training
+
+kafka/bin/kafka-server-start.sh \
+    kafka/config/server.properties
---
+++ src/main/resources/run-zookeeper.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cd ~/kafka-training
+
+kafka/bin/zookeeper-server-start.sh \
+   kafka/config/zookeeper.properties
